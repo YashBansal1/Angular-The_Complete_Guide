6219680324d4e072e3823a362e86b57cf3c10f31
Yash Bansal <Yash.Bansal@wissen.com>
2024-05-28 15:58:19 +0530

    Using lifecycle hooks, Complete Components and Databiding deep diving

--- 9.cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ 9.cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,5 +6,36 @@ import { Component, Input } from '@angular/core';
   styleUrl: './server-element.component.css',
 })
 export class ServerElementComponent {
+  ngOnInit() {
+    console.log('onInit called');
+  }
+
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges() {
+    console.log('onChange called');
+  }
+
+  ngOnCheck() {
+    console.log('onCheck called');
+  }
+
+  ngOnViewInit() {
+    console.log('onViewInit called');
+  }
+
+  ngOnViewChanges() {
+    console.log('onViewChanges called');
+  }
+
+  ngOnContentInit() {
+    console.log('onContentInit called');
+  }
+
+  ngOnContentChanges() {
+    console.log('onContentChanges called');
+  }
   @Input('srvElement') element: { type: string; name: string; content: string };
 }
